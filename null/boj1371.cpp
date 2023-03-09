2cfb5052b1e91350ecac9f78e066dfb095323662
tadminster <91bottlenine@naver.com>
2023-03-10 04:02:02 +0900

    BOJ B2 1371 가장 많은 글자 in progress..2

--- null/boj1371.cpp
+++ null/boj1371.cpp
@@ -5,14 +5,13 @@ using namespace std;
 
 int main()
 {
+	// 알파벳 소문자 map
 	unordered_map <char, int> alphabet;
-
 	for (int i = 97; i < 123; i++)
 		alphabet.insert({i, 0});
 		//alphabet.insert(make_pair(i, 0));
 
-
-
+	// 문자열 입력
 	string str;
 	while (getline(std::cin, str))
 	{
@@ -25,8 +24,10 @@ int main()
 		}
 	}
 
+	// EOF
 	if (std::cin.eof()) {}
 
+	// 전체 출력
 	for (auto& const x : alphabet)
 		cout << x.first << " = " << x.second << endl;
 }
